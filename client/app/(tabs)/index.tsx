b0a760cbbdb365c4fc335128807aef1dc73173a1
Jiya M <jiyam@digitalskillsforall.org>
2025-10-04 14:42:13 -0700

    text and img added

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View, FlatList } from "react-native";
+import { Text, View, FlatList, Image } from "react-native";
 import { useState, useEffect } from "react";
 
 type Branch = {
@@ -26,18 +26,50 @@ export default function HomeScreen() {
 
   return (
 <View style={{ padding: 20, backgroundColor: "#232323", flex: 1 }}>
-  <Text style={{ paddingTop: 30, paddingLeft: 40, fontSize: 70, lineHeight: 65, fontFamily: "Montserrat", color: "white" }}>
-    <Text style={{ fontWeight: "bold" }}>M</Text>USCAT
-  </Text>
-  <Text style={{ paddingLeft: 40, fontSize: 70, lineHeight: 65, fontFamily: "Montserrat", color: "white" }}>
-    <Text style={{ fontWeight: "bold" }}>C</Text>RICKET
-  </Text>
-  <Text style={{ paddingLeft: 40, fontSize: 70, lineHeight: 65, fontFamily: "Montserrat", color: "white" }}>
-    <Text style={{ fontWeight: "bold" }}>C</Text>OACHING
-  </Text>
-  <Text style={{ paddingLeft: 40, fontSize: 70, lineHeight: 65, fontFamily: "Montserrat", color: "white" }}>
-    <Text style={{ fontWeight: "bold" }}>C</Text>ENTER
-  </Text>
+  {/* Row container: puts text and image side by side */}
+  <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+      <View
+  style={{
+    width: 10,
+    height: 290,
+    backgroundColor: "#BD1F14",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 30,
+    marginTop: 45,
+  }}
+>
+</View>
+    {/* Text column */}
+    <View>
+      <Text style={{ paddingTop: 60, paddingLeft: 10, fontSize: 75, lineHeight: 65, fontFamily: "Montserrat", color: "white" }}>
+        <Text style={{ fontWeight: "bold" }}>M</Text>USCAT
+      </Text>
+      <Text style={{ paddingLeft: 10, fontSize: 75, lineHeight: 75, fontFamily: "Montserrat", color: "white" }}>
+        <Text style={{ fontWeight: "bold" }}>C</Text>RICKET
+      </Text>
+      <Text style={{ paddingLeft: 10, fontSize: 75, lineHeight: 75, fontFamily: "Montserrat", color: "white" }}>
+        <Text style={{ fontWeight: "bold" }}>C</Text>OACHING
+      </Text>
+      <Text style={{ paddingLeft: 10, fontSize: 75, lineHeight: 75, fontFamily: "Montserrat", color: "white" }}>
+        <Text style={{ fontWeight: "bold" }}>C</Text>ENTER
+      </Text>
+    </View>
+
+    {/* Image on the right */}
+    <Image
+      source={require("../../images/cricketman.png")}
+      style={{
+        width: 857,
+        height: 857,
+        paddingRight: 60,
+        resizeMode: "contain",
+        marginRight: 20,
+        paddingBottom: 30,
+      }}
+    />
+  </View>
       {/* <FlatList 
         data={branchs}
         keyExtractor={(item) => item.BranchID.toString()}
